d9de3762ca6d6618e9edeefb94eec8ce09db6049
Byron Mackay <byron.mackay@xevant.com>
2024-12-20 14:40:13 -0700

    database url env var created

--- db_config.py
+++ db_config.py
@@ -3,12 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-# Database URL - modify these values according to your setup
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-
 # Create the database URL
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
