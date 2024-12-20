168b8d14da7c10fd4db4bda884dc334752a5dece
Byron Mackay <byron.mackay@xevant.com>
2024-12-20 14:52:18 -0700

    database url env var created

--- database.py
+++ database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 import os
 
+print("DATABASE_URL:")
+print(os.getenv("DATABASE_URL"))
 engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
